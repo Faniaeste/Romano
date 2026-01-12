1246701e9c1c2d197ad16d0923cdf3e2ad57d331
Estefania Sanchez Sanchez <faniaeste92@gmail.com>
2026-01-12 18:53:28 +0100

    actualización de ayudas

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+1- Crear una función que pase de entero >0 y <4000 a romano.
+2- Cualquier otra entrada da error.
+3- Limite 3999.
+
+casos prueba
+a-1994 -> MCMXCIV
+b-4000 ->RomanNumberError() el valor debe ser menor a 4000
+c-"una cadena" -> RomanNumberError
+
+M -> 1000
+D -> 500
+C -> 100
+L -> 50
+X -> 10
+V -> 5
+I -> 1
+"""
