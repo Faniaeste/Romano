81752da11c529286ee27115dfe3e740c041d989f
Estefania Sanchez Sanchez <faniaeste92@gmail.com>
2026-01-14 13:04:16 +0100

    Recolocando la estructura

--- main.py
+++ main.py
@@ -15,13 +15,30 @@ L -> 50
 X -> 10
 V -> 5
 I -> 1
-"""
+
 diccionario = {1000:'M', 500:'D', 100:'C', 50:'L', 10:'X', 5:'V', 1:'I'}
 unidades ={1:'I', 2:'II', 3: 'III',4:'IV', 5:'V', 6:'VI', 7:'VII', 8:'VIII', 9:'VX'}
 decenas = {10:'X',20:'XX',30:'XXX',40:'XL',50:'L',60:'LX',70:'LXX',80:'LXXX',90:'XC'}
 centenas = {100:'C',200:'CC',300:'CCC',400:'CD',500:'D',600:'DC',700:'DCC',800:'DCCC',900:'CM'}
 millares ={1000:'M', 2000:'MM', 3000:'MMM'}
-
+"""
+#--------------------------PRIMERA PRUEBA FALLIDA-----------------------------------------------------------------
+"""
+    for i in range(0,len(numero_list)):
+        if i == 0:
+            numero_list[i] = int(numero_list[i]) * 1000
+            valor_romano += millares.get(numero_list[i])#M
+        if i == 1:
+            numero_list[i] = int(numero_list[i]) * 100
+            valor_romano += centenas.get(numero_list[i])#CM
+        if i == 2:
+            numero_list[i] = int(numero_list[i]) * 10
+            valor_romano += decenas.get(numero_list[i])#XC
+        if i == 3:
+            numero_list[i] = int(numero_list[i])
+            valor_romano += unidades.get(numero_list[i])#IV
+""" 
+#--------------------------PRIMERA PRUEBA FALLIDA-----------------------------------------------------------------
 dic_entero_a_romano = {
     1:'I', 2:'II', 3: 'III',4:'IV', 5:'V', 6:'VI', 7:'VII', 8:'VIII', 9:'VX'
     10:'X',20:'XX',30:'XXX',40:'XL',50:'L',60:'LX',70:'LXX',80:'LXXX',90:'XC'
@@ -41,33 +58,14 @@ def entero_a_romano(numero):
     valor_num = 1000
     while cont < len(numero_list):
         numero_list[cont] = int(numero_list[cont]) * valor_num
+        valor_romano += dic_entero_a_romano.get(numero_list[cont])
         cont += 1
         valor_num /= 10
-#--------------------------PRIMERA PRUEBA FALLIDA-----------------------------------------------------------------
-"""
-    for i in range(0,len(numero_list)):
-        if i == 0:
-            numero_list[i] = int(numero_list[i]) * 1000
-            valor_romano += millares.get(numero_list[i])#M
-        if i == 1:
-            numero_list[i] = int(numero_list[i]) * 100
-            valor_romano += centenas.get(numero_list[i])#CM
-        if i == 2:
-            numero_list[i] = int(numero_list[i]) * 10
-            valor_romano += decenas.get(numero_list[i])#XC
-        if i == 3:
-            numero_list[i] = int(numero_list[i])
-            valor_romano += unidades.get(numero_list[i])#IV
-""" 
-#--------------------------PRIMERA PRUEBA FALLIDA-----------------------------------------------------------------
-"""          
-print(numero_list)#['1','9','9','4']
 
+       
+    print(numero_list)#['1','9','9','4']
 
-
-
-
-return valor_romano
+    return valor_romano
 
 print(entero_a_romano(1994))
 
