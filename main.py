0d0bfcf9d90305ba7e53cae9a337301a657c730d
Estefania Sanchez Sanchez <faniaeste92@gmail.com>
2026-01-15 13:08:59 +0100

    Primera versión de entero a romano

--- main.py
+++ main.py
@@ -37,13 +37,24 @@ millares ={1000:'M', 2000:'MM', 3000:'MMM'}
         if i == 3:
             numero_list[i] = int(numero_list[i])
             valor_romano += unidades.get(numero_list[i])#IV
-""" 
+        
+            if len(numero) == 3:
+                numero = '0' + numero
+            elif len(numero) == 2:
+                numero = '00' + numero
+            elif len(numero) == 1:
+                numero = '000' + numero
+            """
+
+ 
 #--------------------------PRIMERA PRUEBA FALLIDA-----------------------------------------------------------------
+dic_romano_a_entero = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+
 dic_entero_a_romano = {
     #0:''
-    1:'I', 2:'II', 3: 'III',4:'IV', 5:'V', 6:'VI', 7:'VII', 8:'VIII', 9:'VX'
-    10:'X',20:'XX',30:'XXX',40:'XL',50:'L',60:'LX',70:'LXX',80:'LXXX',90:'XC'
-    100:'C',200:'CC',300:'CCC',400:'CD',500:'D',600:'DC',700:'DCC',800:'DCCC',900:'CM'
+    1:'I', 2:'II', 3: 'III',4:'IV', 5:'V', 6:'VI', 7:'VII', 8:'VIII', 9:'VX',
+    10:'X',20:'XX',30:'XXX',40:'XL',50:'L',60:'LX',70:'LXX',80:'LXXX',90:'XC',
+    100:'C',200:'CC',300:'CCC',400:'CD',500:'D',600:'DC',700:'DCC',800:'DCCC',900:'CM',
     1000:'M', 2000:'MM', 3000:'MMM'}
 
 class RomanNumberError( Exception ):
@@ -56,22 +67,15 @@ def romano_a_entero(romano:str)->int:#'III'
     for i in list_romano:
         valor_entero = valor_entero + dic_romano_a_entero.get(i)
 
-    pass
+    return valor_entero
+print(romano_a_entero('MDCCXIII'))
 
 
 def entero_a_romano(numero:int )->str:
     #numero = str(numero)#Transformar en cadena el valor (numero)
     numero = "{:0>4d}".format(numero)
-"""
-if len(numero) == 3:
-    numero = '0' + numero
-elif len(numero) == 2:
-    numero = '00' + numero
-elif len(numero) == 1:
-    numero = '000' + numero
-"""
 
-    numero_list = list(numero)#Guardar una lista['1','9','9','4']
+    numero_list = list(numero) #Guardar una lista['1','9','9','4']
     print(numero_list)
     valor_romano = ''
     cont = 0
--- test_pruebas_unitarias_entero_a_romanos.py
+++
@@ -1,6 +0,0 @@
-from main import entero_a_romano
-
-def test_romano_a_entero_I():
-    assert romano_a_entero('I') == 1
-
-def test
