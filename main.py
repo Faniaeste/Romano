400a390c3b47a0831bd437bcba8e5225f86b693c
Estefania Sanchez Sanchez <faniaeste92@gmail.com>
2026-01-15 13:44:59 +0100

    Avanzando con el reto

--- main.py
+++ main.py
@@ -51,7 +51,6 @@ millares ={1000:'M', 2000:'MM', 3000:'MMM'}
 dic_romano_a_entero = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
 
 dic_entero_a_romano = {
-    #0:''
     1:'I', 2:'II', 3: 'III',4:'IV', 5:'V', 6:'VI', 7:'VII', 8:'VIII', 9:'VX',
     10:'X',20:'XX',30:'XXX',40:'XL',50:'L',60:'LX',70:'LXX',80:'LXXX',90:'XC',
     100:'C',200:'CC',300:'CCC',400:'CD',500:'D',600:'DC',700:'DCC',800:'DCCC',900:'CM',
@@ -64,11 +63,15 @@ class RomanNumberError( Exception ):
 def romano_a_entero(romano:str)->int:#'III'
     list_romano = list(romano)#['I','I','I']
     valor_entero = 0
-    for i in list_romano:
-        valor_entero = valor_entero + dic_romano_a_entero.get(i)
+    for i in range(0,len(list_romano)):
+        if i == 0:
+            if dic_romano_a_entero.get(list_romano[i]) < dic_romano_a_entero.get(list_romano[i+1]):
+                valor_entero = dic_romano_a_entero.get(list_romano[i+1]) - dic_romano_a_entero.get(list_romano[i])
+        else: 
+            valor_entero = valor_entero + dic_romano_a_entero.get(list_romano[i])
 
     return valor_entero
-print(romano_a_entero('MDCCXIII'))
+print(romano_a_entero('IV'))
 
 
 def entero_a_romano(numero:int )->str:
@@ -76,7 +79,7 @@ def entero_a_romano(numero:int )->str:
     numero = "{:0>4d}".format(numero)
 
     numero_list = list(numero) #Guardar una lista['1','9','9','4']
-    print(numero_list)
+    #print(numero_list)
     valor_romano = ''
     cont = 0
     valor_num = 1000
